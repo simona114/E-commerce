f96344bc2a9a83e858fae154bfb8f935d10ad56a
Simona Nikolova <57250076+simona114@users.noreply.github.com>
2023-02-12 20:06:27 +0200

    Create product item

---
+++ app/src/main/java/com/example/e_commerce/data/ProductCategory.kt
@@ -0,0 +1,5 @@
+package com.example.e_commerce.data
+
+enum class ProductCategory {
+ELECTRONICS, JEWELRY, MEN_CLOTHING, WOMEN_CLOTHING
+}
---
+++ app/src/main/java/com/example/e_commerce/data/models/ProductModel.kt
@@ -0,0 +1,5 @@
+package com.example.e_commerce.data.models
+
+import com.example.e_commerce.data.ProductCategory
+
+data class ProductModel (val title:String, val price:Double, val category: ProductCategory, val image:String)
