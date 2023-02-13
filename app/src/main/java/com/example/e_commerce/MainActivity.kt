eed19b8b8f33c53ab0e55fb46132cd621c197701
Simona Nikolova <57250076+simona114@users.noreply.github.com>
2023-02-13 12:47:34 +0200

    Set up bottom navigation

--- app/src/main/java/com/example/e_commerce/MainActivity.kt
+++ app/src/main/java/com/example/e_commerce/MainActivity.kt
@@ -1,26 +1,74 @@
 package com.example.e_commerce
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.e_commerce.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    lateinit var binding:ActivityMainBinding;
+    lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
         setNavigation()
     }
 
-    fun setNavigation(){
+    private fun setNavigation() {
+        val bottomNavView: BottomNavigationView = binding.bottomNavigation
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
         val navController = navHostFragment.navController
+
+        //Enable navigation via back arrow on phone
+        navController.enableOnBackPressed(true)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment, R.id.cartFragment, R.id.favoritesFragment, R.id.profileFragment
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        bottomNavView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+
+                R.id.homeFragment -> {
+                    binding.bottomNavigation.visibility = View.VISIBLE
+
+                }
+                R.id.cartFragment -> {
+                    binding.bottomNavigation.visibility = View.VISIBLE
+                }
+                R.id.favoritesFragment -> {
+                    binding.bottomNavigation.visibility = View.VISIBLE
+                }
+                R.id.profileFragment -> {
+                    binding.bottomNavigation.visibility = View.VISIBLE
+                }
+                else -> {
+                    binding.bottomNavigation.visibility = View.GONE
+
+                }
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
