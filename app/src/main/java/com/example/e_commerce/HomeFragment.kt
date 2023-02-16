21548f0f984cfd70a144ae6b22d534805c6bc0f6
Simona Nikolova <57250076+simona114@users.noreply.github.com>
2023-02-16 21:44:11 +0200

    Filter products by category

--- app/src/main/java/com/example/e_commerce/HomeFragment.kt
+++ app/src/main/java/com/example/e_commerce/HomeFragment.kt
@@ -1,9 +1,15 @@
 package com.example.e_commerce
 
+import android.opengl.Visibility
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
+import androidx.core.view.children
+import androidx.core.view.isVisible
+import androidx.core.widget.CompoundButtonCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,7 +17,10 @@ import com.example.e_commerce.data.ProductAdapter
 import com.example.e_commerce.data.ProductCategory
 import com.example.e_commerce.data.models.ProductModel
 import com.example.e_commerce.databinding.FragmentHomeBinding
+import com.google.android.material.chip.Chip
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
+import kotlin.collections.ArrayList
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
@@ -30,22 +39,77 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         val products = listOf(
-            ProductModel("phone", 800.00, ProductCategory.ELECTRONICS, "https://i.dummyjson.com/data/products/1/4.jpg"),
+            ProductModel(
+                "phone",
+                800.00,
+                ProductCategory.ELECTRONICS,
+                "https://i.dummyjson.com/data/products/1/4.jpg"
+            ),
             ProductModel("Iphone", 1800.00, ProductCategory.ELECTRONICS, "url"),
-            ProductModel("Iphone", 1800.00, ProductCategory.ELECTRONICS, "url"),
-            ProductModel("Iphone", 1800.00, ProductCategory.ELECTRONICS, "url")
+            ProductModel("Iphone", 1800.00, ProductCategory.MEN_CLOTHING, "url"),
+            ProductModel("Jewelry", 1800.00, ProductCategory.JEWELRY, "url")
         )
+
+        val checkedChangeListener =
+            CompoundButton.OnCheckedChangeListener { _, _ ->
+                filterList(products)
+            }
+
+        binding.apply {
+            cCategoryElectronics.setOnCheckedChangeListener(checkedChangeListener)
+            cCategoryJewelry.setOnCheckedChangeListener(checkedChangeListener)
+            cCategoryWomenClothing.setOnCheckedChangeListener(checkedChangeListener)
+            cCategoryMenClothing.setOnCheckedChangeListener(checkedChangeListener)
+        }
 
         repoAdapter = ProductAdapter()
         repoAdapter?.injectList(products)
 
         binding.rvProducts.apply {
             adapter = repoAdapter
-            layoutManager = LinearLayoutManager(activity,  LinearLayoutManager.HORIZONTAL, false)
+            layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
         }
+
+
     }
 
+    private fun filterList(products: List<ProductModel>) {
+        val filteredProducts = ArrayList<ProductModel>()
 
+        val chipGroup = binding.cgProductCategories
+        val selectedChipsTitles = chipGroup.children
+            .filter { (it as Chip).isChecked }
+            .map { (it as Chip).text.toString().lowercase() }.toList()
+
+        if (selectedChipsTitles.isEmpty()) {
+            binding.apply {
+                tvNoProducts.visibility = View.GONE
+                rvProducts.visibility = View.VISIBLE
+            }
+            repoAdapter?.injectList(products)
+        } else {
+            for (product in products) {
+                if (product.category.name.lowercase() in selectedChipsTitles) {
+                    filteredProducts.add(product)
+                }
+            }
+
+            if (filteredProducts.isEmpty()) {
+                binding.apply {
+                    rvProducts.visibility = View.GONE
+                    tvNoProducts.visibility = View.VISIBLE
+                }
+
+            } else {
+                binding.apply {
+                    rvProducts.visibility = View.VISIBLE
+                    tvNoProducts.visibility = View.GONE
+                }
+                repoAdapter?.injectList(filteredProducts)
+            }
+        }
+    }
 }
+
+
--- app/src/main/java/com/example/e_commerce/data/ProductAdapter.kt
+++ app/src/main/java/com/example/e_commerce/data/ProductAdapter.kt
@@ -2,6 +2,8 @@ package com.example.e_commerce.data
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.e_commerce.R
@@ -9,8 +11,9 @@ import com.example.e_commerce.data.models.ProductModel
 import com.example.e_commerce.databinding.ItemProductBinding
 
 class ProductAdapter() :
-    RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
+    RecyclerView.Adapter<ProductAdapter.ProductViewHolder>(){
     var productList = listOf<ProductModel>()
+    var productListFiltered = listOf<ProductModel>()
 
     inner class ProductViewHolder(private val productsBinding: ItemProductBinding) :
         RecyclerView.ViewHolder(productsBinding.root) {
@@ -24,7 +27,7 @@ class ProductAdapter() :
                     .placeholder(R.drawable.placeholder)
                     .error(R.drawable.placeholder)
                     .centerCrop()
-                    .into(ivProductImage);
+                    .into(ivProductImage)
             }
         }
     }
@@ -46,5 +49,35 @@ class ProductAdapter() :
         this.productList = productList
         notifyDataSetChanged()
     }
-
+//    todo: use for searching products
+//    override fun getFilter(): Filter {
+//        return object : Filter() {
+//            override fun performFiltering(constraint: CharSequence?): FilterResults {
+//                val charString = constraint?.toString()?.lowercase() ?: ""
+//                if (charString.isEmpty()) productListFiltered = productList else {
+//                    val filteredList = ArrayList<ProductModel>()
+//                    productList
+//                        .filter {
+//                            it.category.name.lowercase().contains(constraint!!)
+//                        }
+//                        .forEach { filteredList.add(it) }
+//                    productListFiltered = filteredList
+//
+//                }
+//
+//                return FilterResults().apply { values = productListFiltered }
+//            }
+//
+//            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+//
+//                productListFiltered = if (results?.values == null)
+//                    ArrayList()
+//                else
+//                    results.values as List<ProductModel>
+//
+//                notifyDataSetChanged()
+//            }
+//        }
+//    }
+//
 }
