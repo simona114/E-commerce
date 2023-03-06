aa603d26dc532f7a28e1e494b044c09087ce7b9a
Simona Nikolova <57250076+simona114@users.noreply.github.com>
2023-03-06 22:08:13 +0200

    Add navigation from Cart screen to Home screen

--- app/src/main/java/com/example/e_commerce/ui/CartFragment.kt
+++ app/src/main/java/com/example/e_commerce/ui/CartFragment.kt
@@ -5,17 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.e_commerce.R
+import com.example.e_commerce.databinding.FragmentCartBinding
+import com.example.e_commerce.databinding.FragmentHomeBinding
 
 
 class CartFragment : Fragment() {
 
+    lateinit var binding: FragmentCartBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+    ): View {
+        binding = FragmentCartBinding.inflate(layoutInflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnBrowseProducts.setOnClickListener {
+            findNavController().navigate(CartFragmentDirections.actionCartFragmentToHomeFragment())
+        }
     }
 
 }
